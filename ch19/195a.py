95e01b74c3654498c9a17a6eac31381b406475fb
Flynn <flynn@local>
2026-04-19 03:06:27 -0700

    Created Fuel Efficient, Horse Power and Price Functions

--- ch19/195a.py
+++ ch19/195a.py
@@ -1,8 +1,4 @@
-# Create function - reads car_data into list
-# - Each element must be a dictionary
-# -- Dictionary keys are the column names
-# --- Values are the elements in each column
-
+"""Accesses the Car Database for arbitrary data on automobiles"""
 import csv
 from pathlib import Path
 
@@ -38,13 +34,45 @@ def carMake(term):
     searchTerm = normalize_text(term)
     make = []
 
-    with open(file_path,newline="",encoding="utf-8") as csv_make:
+    with open(file_path, newline="", encoding="utf-8") as csv_make:
         reader = csv.DictReader(csv_make)
-        for row in reader:
-            if searchTerm in normalize_text(row["make"]):
-                make.append(f"{row['make']} {row['body_style']} {row['price']}")
+        results = filter(lambda row: searchTerm in normalize_text(row["make"]), reader,)
+        for row in results:    
+            make.append(f"{row['make']} {row['body_style']} {row['price']}")
     print(make)
     return make
 
-carMake("audi")
+def fuelEfficient():
+    matches = []
+    
+    with open(file_path, newline="", encoding="utf-8") as csv_fuelefficient:
+        reader = csv.DictReader(csv_fuelefficient)
+        results = filter(lambda row: row["city_mpg"].isdigit() and int(row["city_mpg"]) >= 35, reader,)
+        
+        for row in results:
+            matches.append(f"{row['make']} {row['body_style']} {row['city_mpg']} mpg")
+    return matches
 
+def horsePower():
+    matches = []
+    with open(file_path, newline="", encoding="utf-8") as csv_hp:
+        reader = csv.DictReader(csv_hp)
+        results = filter(lambda row: row["horsepower"].isdigit() and int(row["horsepower"]) >= 100, reader,)
+        
+        for row in results:
+            matches.append(f"{row['make']} {row['body_style']} {row['horsepower']} hp")
+    return matches
+
+def price(inputPrice):
+    matches = []
+    inputPrice = int(inputPrice)
+    with open(file_path, newline="", encoding="utf-8") as csv_prices:
+        reader = csv.DictReader(csv_prices)
+        results = filter(lambda row: row["price"].isdigit() and int(row["price"]) <= inputPrice, reader,)
+        
+        for row in results:
+            matches.append(f"Price: {row['price']} Make: {row['make']} Body: {row['body_style']} HP: {row['horsepower']} MPG: {row['city_mpg']}")
+        return matches
+
+cinMain = input("Please enter the budget you broke bastard: ")
+print(price(cinMain))
